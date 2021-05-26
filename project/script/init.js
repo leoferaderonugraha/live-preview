1c48c9a329f1d12ba05fad923328f37675c491d7
Leo Feradero Nugraha <leoferaderongraha@gmail.com>
2021-05-26 19:03:56 +0700

    Revert Back

--- project/script/init.js
+++ project/script/init.js
@@ -1,26 +1,6 @@
-// ReactDOM.render(
-//     <div>
-//         <div className="container">
-//             <div className="row">
-//                 <carouselGallery
-//                     previewImages={project["previewImages"]}
-//                 />
-//                 <ProjectWrapper
-//                     project={project}
-//                 />
-//             </div>
-//         </div>
-//         <hr className="mt-5 mb-5" />
-//         <div className="container">
-//             <projectDescDetails
-//                 description={project["description"]}
-//                 address={project["address"]}
-//                 mapImageUrl={project["mapImageUrl"]}
-//             />
-//         </div>
-//     </div>,
 function initReact(project) {
     ReactDOM.render(
+        // wrapper
         React.createElement("div", null,
             React.createElement("div",
                 {
