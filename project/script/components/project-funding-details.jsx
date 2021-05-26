57f55fcc69810fa9bc541d350c203b2303ed8705
Leo Feradero Nugraha <leoferaderongraha@gmail.com>
2021-05-27 01:37:19 +0700

    Change to babel

---
+++ project/script/components/project-funding-details.jsx
@@ -0,0 +1,30 @@
+const projectFundingDetails = (props) => {
+    return (
+        <div className="row">
+            <div className="col">
+                <div className="row">
+                    <p className="detail" id="funding-progress">
+                        Rp. {props.fundingProgress}
+                    </p>
+                </div>
+                <div className="row">
+                    <p className="sub-detail" id="total-funding">
+                        dari Rp. {props.totalFunding}
+                    </p>
+                </div>
+            </div>
+            <div className="col">
+                <div className="row">
+                    <p className="detail" id="remaining-days">
+                        {props.remainingDays}
+                    </p>
+                </div>
+                <div className="row">
+                    <p className="sub-detail">
+                        Hari Lagi
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+};
