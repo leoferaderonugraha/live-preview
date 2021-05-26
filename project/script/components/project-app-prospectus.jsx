9ef273f3cf8b3bdd4ec92afe3c8eb7796c9ba927
Leo Feradero Nugraha <leoferaderongraha@gmail.com>
2021-05-26 21:14:19 +0700

    Change to babel

--- project/script/components/project-app-prospectus.jsx
+++ project/script/components/project-app-prospectus.jsx
@@ -3,14 +3,14 @@ const projectAppProspectus = (props) => {
         <div className="row row-buttons">
             <div className="col">
                 <a href="https://play.google.com/store/apps/details?id=store.numoney.landxapp" id="btn-invest">
-                    <button className="btn btn-primary btn-detail-bottom" style="width: 100%; box-shadow: 0 8px 10px 1px rgba(0, 0, 0, 0.14)">
+                    <button className="btn btn-primary btn-detail-bottom" style={{width: "100%", boxShadow: "0 8px 10px 1px rgba(0, 0, 0, 0.14)"}}>
                         BELI VIA APP
                     </button>
                 </a>
             </div>
             <div className="col">
                 <a href={props.prospectusLink} id="download-prospectus">
-                    <button className="btn btn-outline-success btn-detail-bottom" style="width: 100%">
+                    <button className="btn btn-outline-success btn-detail-bottom" style={{width: "100%"}}>
                         DOWNLOAD PROSPEKTUS
                     </button>
                 </a>
