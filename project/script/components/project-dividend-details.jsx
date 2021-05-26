6fb965a9cebd149495f9d88ea42604971acc321a
Leo Feradero Nugraha <leoferaderongraha@gmail.com>
2021-05-27 01:21:56 +0700

    Change to babel

--- project/script/components/project-dividend-details.jsx
+++ project/script/components/project-dividend-details.jsx
@@ -6,14 +6,14 @@ const projectDividendDetails = (props) => {
                     <div className="col-auto pl-0">
                         <span className="detail-2" style={{display: "inline"}}>
                             Periode Dividen 
+                            <i className="fa fa-info-circle" 
+                                style={{fontSize: "12px", color: "seagreen"}}
+                                data-toggle="tooltip"
+                                id="dividend-period-desc"
+                                data-original-title=""
+                                title="Periode dividen aktual akan tetap mengacu pada laporan keuangan dan persetujuan Rapat Umum Pemegang Saham (RUPS).">
+                            </i>
                         </span>
-                        <i className="fa fa-info-circle" 
-                            style={{fontSize: "12px", color: "seagreen"}}
-                            data-toggle="tooltip"
-                            id="dividend-period-desc"
-                            data-original-title=""
-                            title="Periode dividen aktual akan tetap mengacu pada laporan keuangan dan persetujuan Rapat Umum Pemegang Saham (RUPS).">
-                        </i>
                     </div>
                 </div>
                 <div className="row">
@@ -27,14 +27,14 @@ const projectDividendDetails = (props) => {
                     <div className="col-auto pl-0">
                         <span className="detail-2" style={{display: "inline"}}>
                             Estimasi Dividen 
+                            <i className="fa fa-info-circle mt-1" 
+                                style={{fontSize: "12px", color: "seagreen"}}
+                                data-toggle="tooltip"
+                                id="dividend-period-desc"
+                                data-original-title=""
+                                title="Disclaimer: kinerja masa lalu bukan merupakan indikasi kinerja masa depan. Estimasi dividen adalah ilustrasi berdasarkan proyeksi konservatif, dividen aktual akan tetap mengacu pada kinerja penerbit dan persetujuan Rapat Umum Pemegang Saham (RUPS). Estimasi dividen belum termasuk potensi capital gain dari kenaikan harga saham">
+                            </i>
                         </span>
-                        <i className="fa fa-info-circle" 
-                            style={{fontSize: "12px", color: "seagreen"}}
-                            data-toggle="tooltip"
-                            id="dividend-period-desc"
-                            data-original-title=""
-                            title="Disclaimer: kinerja masa lalu bukan merupakan indikasi kinerja masa depan. Estimasi dividen adalah ilustrasi berdasarkan proyeksi konservatif, dividen aktual akan tetap mengacu pada kinerja penerbit dan persetujuan Rapat Umum Pemegang Saham (RUPS). Estimasi dividen belum termasuk potensi capital gain dari kenaikan harga saham">
-                        </i>
                     </div>
                 </div>
                 <div className="row">
