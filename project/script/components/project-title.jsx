63270ae3801255a3c78f4b1260e81b2e4b8f618f
Leo Feradero Nugraha <leoferaderongraha@gmail.com>
2021-05-27 02:25:15 +0700

    Change to babel

---
+++ project/script/components/project-title.jsx
@@ -0,0 +1,16 @@
+const projectTitle = (props) => {
+    return (
+        <div className="row-cols-1">
+            <div className="row">
+                <p className="main-title" id="project-name">
+                    {props.name}
+                </p>
+            </div>
+            <div className="row">
+                <p className="main-title" id="project-issuername">
+                    - {props.issuerName}
+                </p>
+            </div>
+        </div>
+    );
+};
