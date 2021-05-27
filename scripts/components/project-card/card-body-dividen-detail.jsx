b87ff053bc8f9e886b1bab7b4ff86057e60e106a
Leo Feradero Nugraha <leoferaderongraha@gmail.com>
2021-05-28 02:26:11 +0700

    Change to babel

--- scripts/components/project-card/card-body-dividen-detail.jsx
+++ scripts/components/project-card/card-body-dividen-detail.jsx
@@ -1,33 +1,33 @@
 const CardBodyDividendDetail = (props) => {
     return(
-        <div class="row">
-            <div class="col">
-                <div class="row">
-                    <div class="col">
-                        <p class="sub-progress-detail">
-                            Periode Dividen <span class="iconify info-outline-circle" data-icon="ant-design:info-circle-outlined" data-toggle="tooltip" title="Hello"></span>
+        <div className="row">
+            <div className="col">
+                <div className="row">
+                    <div className="col">
+                        <p className="sub-progress-detail">
+                            Periode Dividen <span className="iconify info-outline-circle" data-icon="ant-design:info-circle-outlined" data-toggle="tooltip" title="Hello"></span>
                         </p>
                     </div>
                 </div>
-                <div class="row">
-                    <div class="col">
-                        <p class="progress-detail-2">
+                <div className="row">
+                    <div className="col">
+                        <p className="progress-detail-2">
                             {props.dividendSchedule}
                         </p>
                     </div>
                 </div>
             </div>
-            <div class="col">
-                <div class="row">
-                    <div class="col">
-                        <p class="sub-progress-detail">
-                            Estimasi Dividen <span class="iconify info-outline-circle" data-icon="ant-design:info-circle-outlined" data-toggle="tooltip" title="Hello"></span>
+            <div className="col">
+                <div className="row">
+                    <div className="col">
+                        <p className="sub-progress-detail">
+                            Estimasi Dividen <span className="iconify info-outline-circle" data-icon="ant-design:info-circle-outlined" data-toggle="tooltip" title="Hello"></span>
                         </p>
                     </div>
                 </div>
-                <div class="row">
-                    <div class="col">
-                        <p class="progress-detail-2">
+                <div className="row">
+                    <div className="col">
+                        <p className="progress-detail-2">
                             {props.annualRentYield}-{props.annualRentYieldUpper}% <span>Per Tahun</span>
                         </p>
                     </div>
