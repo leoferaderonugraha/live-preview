ad0379179dc26c82528acc1e5d60750afcba6c4f
Leo Feradero Nugraha <leoferaderongraha@gmail.com>
2021-05-28 02:04:40 +0700

    Change to babel

--- scripts/components/project-card/create-card.jsx
+++ scripts/components/project-card/create-card.jsx
@@ -2,15 +2,7 @@ function CreateCard(props) {
     ReactDOM.render(
         <div className="container">
             <div className="row">
-                <CardBase
-                    id={props.id}
-                    totalImages={props.previewImages.length}
-                    previewImages={props.previewImages}
-                    projectName={props.projectName}
-                    category={props.category}
-                    lotPrice={props.lotPrice}
-                    totalLot={props.totalLot}
-                />
+                {CardBase(props)}
             </div>
         </div>
 
