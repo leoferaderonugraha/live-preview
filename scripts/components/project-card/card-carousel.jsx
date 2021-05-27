bdabba48435b328c38758c5738a2b9e676a37753
Leo Feradero Nugraha <leoferaderongraha@gmail.com>
2021-05-27 22:03:25 +0700

    Change to babel

--- scripts/components/project-card/card-carousel.jsx
+++ scripts/components/project-card/card-carousel.jsx
@@ -2,7 +2,7 @@ const CardCarousel = (props) => {
     return (
         <div id={props.id} className="carousel slide" data-ride="carousel" data-interval="false">
             <ol className="carousel-indicators">
-                <CardCarouselTarget totalImages={10}/>
+                <CardCarouselTarget totalImages={10} id={props.id}/>
                 <CardCarouselInner />
             </ol>
         </div>
