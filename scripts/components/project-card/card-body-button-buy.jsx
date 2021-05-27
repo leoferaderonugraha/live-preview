a03eaff1bebdc507872dba18b87dbeb1785b837b
Leo Feradero Nugraha <leoferaderongraha@gmail.com>
2021-05-27 23:51:43 +0700

    Change card styling

--- scripts/components/project-card/card-body-button-buy.jsx
+++ scripts/components/project-card/card-body-button-buy.jsx
@@ -2,7 +2,7 @@ const CardBodyButtonBuy = () => {
     return (
         <div className="row">
             <div className="col">
-                <button className="btn project-buy">BELI</button>
+                <button className="btn project-button-buy">BELI</button>
             </div>
         </div>
     );
