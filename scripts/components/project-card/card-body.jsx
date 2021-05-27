2732a21b6ecc1de0783063bcec368471f5365817
Leo Feradero Nugraha <leoferaderongraha@gmail.com>
2021-05-28 00:40:49 +0700

    Change card styling

--- scripts/components/project-card/card-body.jsx
+++ scripts/components/project-card/card-body.jsx
@@ -1,6 +1,6 @@
 const CardBody = (props) => {
     return (
-        <div className="card-body">
+        <div className="card-body custom">
             <div className="container-fluid px-0">
                 <CardBodyButtonBuy />
                 <CardBodySymbol />
