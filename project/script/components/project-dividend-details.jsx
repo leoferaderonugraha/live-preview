a6ce80b8f5533fcd3e17cfe918b430ef358c0e9d
Leo Feradero Nugraha <leoferaderongraha@gmail.com>
2021-05-28 03:09:03 +0700

    Update styling

--- project/script/components/project-dividend-details.jsx
+++ project/script/components/project-dividend-details.jsx
@@ -7,7 +7,7 @@ const ProjectDividendDetails = (props) => {
                         <p className="detail-2" style={{display: "inline"}}>
                             Periode Dividen <i className="iconify" data-icon="ant-design:info-circle-outlined"
                                                 data-toggle="tooltip"
-                                                style={{fontSize: "12px", color: "seagreen"}}
+                                                style={{fontSize: "18px", color: "seagreen"}}
                                                 title="Periode dividen aktual akan tetap mengacu pada laporan keuangan dan persetujuan Rapat Umum Pemegang Saham (RUPS).">
                                             </i>
                         </p>
@@ -25,7 +25,7 @@ const ProjectDividendDetails = (props) => {
                         <p className="detail-2" style={{display: "inline"}}>
                             Estimasi Dividen <i className="iconify" data-icon="ant-design:info-circle-outlined"
                                                 data-toggle="tooltip"
-                                                style={{fontSize: "12px", color: "seagreen"}}
+                                                style={{fontSize: "18px", color: "seagreen"}}
                                                 title="Disclaimer: kinerja masa lalu bukan merupakan indikasi kinerja masa depan. Estimasi dividen adalah ilustrasi berdasarkan proyeksi konservatif, dividen aktual akan tetap mengacu pada kinerja penerbit dan persetujuan Rapat Umum Pemegang Saham (RUPS). Estimasi dividen belum termasuk potensi capital gain dari kenaikan harga saham.">
                                             </i>
                         </p>
